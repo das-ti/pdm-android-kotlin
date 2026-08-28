8a53a7138ecb0e8ac3549ba42269b482210ae1d1
das-ti <das.ti@outlook.com.br>
2026-08-28 00:25:30 -0300

    Revert "fix: correção de funções da segunda tela"
    This reverts commit a00a842001d66012ee7cd655c45aafd023f939da.

--- CadastroAlunosApp/app/src/main/java/com/mylabsystems/cadastroalunosapp/MatriculaAluno.kt
+++ CadastroAlunosApp/app/src/main/java/com/mylabsystems/cadastroalunosapp/MatriculaAluno.kt
@@ -1,27 +1,20 @@
 package com.mylabsystems.cadastroalunosapp
-
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlin.random.Random
 
 class MatriculaAluno : AppCompatActivity(R.layout.activity_matricula_aluno) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val txvNomeAluno = findViewById<TextView>(R.id.txv_nome_aluno)
-        val txvMatriculaAluno = findViewById<TextView>(R.id.txv_matricula_aluno)
+        val txv = findViewById<TextView>(R.id.txv)
+        val txvAutorLivro = findViewById<TextView>(R.id.txv_autor_livro)
         val fabVoltar = findViewById<FloatingActionButton>(R.id.fab_voltar)
-        val btnMatricular = findViewById<Button>(R.id.btn_matricular)
 
-        btnMatricular.setOnClickListener {
-            val nomeAluno = intent.getStringExtra("nome_aluno")
-            val numeroMatricula = Random.nextInt(1, 1000)
-
-            txvNomeAluno.text = "Nome: $nomeAluno"
-            txvMatriculaAluno.text = "Matrícula: $numeroMatricula"
-        }
+        val tituloCadastrado = intent.getStringExtra("titulo_livro")
+        val autorCadastrado = intent.getStringExtra("autor_livro")
+        txvTituloLivro.text = "Título: $tituloCadastrado"
+        txvAutorLivro.text = "Autor: $autorCadastrado"
 
         fabVoltar.setOnClickListener {
             finish()
