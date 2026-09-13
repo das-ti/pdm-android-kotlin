1acd02f32cd38a20910a51b26d378c88fd6beb4f
das-ti <das.ti@outlook.com.br>
2026-09-13 03:10:54 -0300

    refactor: remover pasta appimc

--- AppIMC/app/src/main/java/com/mylabsystems/appimc/model/IMC.kt
+++
@@ -1,8 +0,0 @@
-package com.mylabsystems.appimc.model
-
-data class IMC(
-    val peso: Double = 0.0,
-    val altura: Double = 0.0,
-    val imcTotal: Double = 0.0,
-    val mensagem: String = ""
-)
